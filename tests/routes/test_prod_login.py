c87450fbd25e93b04ae1e0b48a3238509b40663f
Mark Bastawros <mark.bastawros@shopify.com>
2020-09-27 22:44:02 -0400

    added tests

--- tests/routes/test_prod_login.py
+++ tests/routes/test_prod_login.py
@@ -1,17 +1,38 @@
 import pytest
 from utils.web import HTTPcore
 from controller.json_web_token.json_web_token import Jwt
+import json
 
 class TestProdLoginApi:
 
-    def test_succsseful_token_validation(self,prod_endpoint):
+    def test_successful_token_validation(self,prod_endpoint):
         core = HTTPcore(prod_endpoint)
         temp = Jwt()
         token = temp.encode_token('john', 'smith', 'john@smith.com', 'admin')
         core.core("GET",headers={"Authorization": token})
         assert core.get_status_code == 200
 
-    def test_unsuccsseful_token_validation(self,prod_endpoint):
+    def test_unsuccessful_token_validation(self,prod_endpoint):
         core = HTTPcore(prod_endpoint)
         core.core("GET",headers={"Authorization": "randomtokennotours12321"})
         assert core.get_status_code == 401
+    
+    def test_successful_login(self,prod_endpoint):
+        web = HTTPcore(prod_endpoint)
+        web.core("POST",data=json.dumps({
+            "email": "test@test.com",
+            "password": "stjosephtest"
+             }))
+        print("res: ",web.json_response)
+        assert web.get_status_code == 200
+        assert web.json_response.get("firstName") == "test"
+        assert web.json_response.get("message") == "Authentication successful"
+
+    def test_unsuccessful_login(self,prod_endpoint):
+        web = HTTPcore(prod_endpoint)
+        web.core("POST",data=json.dumps({
+            "email": "test@test.com",
+            "password": "random"
+             }))
+        assert web.get_status_code == 401
+    
